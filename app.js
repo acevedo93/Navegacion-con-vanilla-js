12f8d4c2919ab0111da0349501418309b133fa6b
David Acevedo <dacevedojaramillo@gmail.com>
2017-07-05 15:41:36 -0300

    navegacion por pestallas con vanilla

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+/*++++++++++++++*/
+
+(function(d){
+    let tabs = Array.prototype.slice.apply( d.querySelectorAll('.tabs_item'));
+    let panels = Array.prototype.slice.apply( d.querySelectorAll('.panel_item'));
+    //delegacion de eventos 
+    //capturando el clik en cada evento
+    d.getElementById('tabs').addEventListener('click', e =>{
+      //
+    
+      if(e.target.classList.contains('tabs_item')){
+       
+        let i = tabs.indexOf(e.target);
+        tabs.map(tabs => tabs.classList.remove('active'));
+        tabs[i].classList.add('active');
+        panels.map(panel => panel.classList.remove('active'));
+        panels[i].classList.add('active');
+        
+    }
+    
+    });
+})(document);
